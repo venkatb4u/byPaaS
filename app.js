73f1a30f21896c7900c777ea3d9687f66e433a92
Venkat <venkat.crescentian@gmail.com>
2017-01-30 11:45:22 +0530

    config n app

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+
+const sweet = require('sweet.js');
+sweet.loadMacro('cspjs');
+
+// Config initialization
+const config = require('./src/config').init();
+
+const routes = require('./routes/index');
+const bypaas = require('./routes/core.sjs');
+
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.use('/bypaas', bypaas); // Core routes
+
+
+// app.use('/:bypaas', function (req, res, next) {
+//   console.log('Request Type:', req.method);
+//   next();
+// });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
